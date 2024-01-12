7067d8c8dcaeb14876c0bb56459571d6cc58d6ef
Hyeonwoo Choi <hyeonwch@c6r4s6.42seoul.kr>
2024-01-12 23:51:38 +0900

    🎨 delete unused functions

--- push_swap/main.c
+++ push_swap/main.c
@@ -1,19 +1,19 @@
 #include "push_swap_checker.h"
 #include <stdio.h>
 
-void    init_dq_a_b(t_deque *dq_a, t_deque *dq_b, int argc, char *argv[])
+void	init_dq_a_b(t_deque *dq_a, t_deque *dq_b, int argc, char *argv[])
 {
-    int i;
+	int i;
 
-    i = 1;
-    init_deque(dq_a, argc); //큐의 가장 앞은 항상 검사를 위해 비어있어야 함
-    init_deque(dq_b, argc);
-    while (i < argc)
-    {
-        if (ft_is_number(*(argv + i)) == ERROR)
-            error_exit(dq_a, dq_b);
-        push_rear(dq_a, ft_atoi_push_swap(*(argv + i++), dq_a, dq_b), argc);
-    }
+	i = 1;
+	init_deque(dq_a, argc); //큐의 가장 앞은 항상 검사를 위해 비어있어야 함
+	init_deque(dq_b, argc);
+	while (i < argc)
+	{
+		if (ft_is_number(*(argv + i)) == ERROR)
+			error_exit(dq_a, dq_b);
+		push_rear(dq_a, ft_atoi_push_swap(*(argv + i++), dq_a, dq_b), argc);
+	}
 }
 
 int main(int argc, char *argv[])
