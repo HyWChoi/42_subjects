54ddbd809ad6c7780938a13ba932b21b86b31358
HyWChoi <109526914+HyWChoi@users.noreply.github.com>
2024-11-22 08:30:55 +0000

    fix cpp01 ex00

--- cpp/cpp01/ex00/main.cpp
+++ cpp/cpp01/ex00/main.cpp
@@ -5,6 +5,7 @@ int	main(){
 	std::cout << "Creating a new Zombie" << std::endl;
 	Zombie *zombie = newZombie("Zombie");
 	zombie->announce();
+	delete zombie;
 
 	std::cout << "Creating a random Zombie" << std::endl;
 	randomChump("RandomZombie");
