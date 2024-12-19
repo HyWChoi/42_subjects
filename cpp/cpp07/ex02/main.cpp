576689a72cb749a1074ed4f0518b9741869a7417
HyWChoi <chw2309@icloud.com>
2024-12-19 18:32:31 +0900

    cpp07

--- cpp/cpp07/ex02/main.cpp
+++ cpp/cpp07/ex02/main.cpp
@@ -1,39 +1,53 @@
+#include <iostream>
 #include "Array.hpp"
 
-int main(){
-	{
-		Array<int> a(5);
-		for (unsigned int i = 0; i < a.size(); i++)
-			a[i] = i;
+#define MAX_VAL 750
+int main(int, char**)
+{
+    Array<int> numbers(MAX_VAL);
+    int* mirror = new int[MAX_VAL];
+    srand(time(NULL));
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        const int value = rand();
+        numbers[i] = value;
+        mirror[i] = value;
+    }
+    //SCOPE
+    {
+        Array<int> tmp = numbers;
+        Array<int> test(tmp);
+    }
 
-		for (unsigned int i = 0; i < a.size(); i++)
-			std::cout << a[i] << std::endl;
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        if (mirror[i] != numbers[i])
+        {
+            std::cerr << "didn't save the same value!!" << std::endl;
+            return 1;
+        }
+    }
+    try
+    {
+        numbers[-2] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    try
+    {
+        numbers[MAX_VAL] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
 
-		try {
-			std::cout << a[6] << std::endl;
-		} catch (std::exception &e) {
-			std::cout << e.what() << std::endl;
-		}
-	}
-	{
-		std::ostringstream ss;
-		std::string test = "test ";
-		Array<std::string> a(5);
-
-		for (unsigned int i = 0; i < a.size(); i++){
-			ss << test << i;
-			a[i] = ss.str();
-			ss.str("");
-		}
-
-		for (unsigned int i = 0; i < a.size(); i++)
-			std::cout << a[i] << std::endl;
-
-		try {
-			std::cout << a[6] << std::endl;
-		} catch (std::exception &e) {
-			std::cout << e.what() << std::endl;
-		}
-	}
-	return 0;
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        numbers[i] = rand();
+    }
+    delete [] mirror;//
+    return 0;
 }
